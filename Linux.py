d9ce20d4cc285104ca59c1e1e94c02f3f8fd6a9f
Phish <78632176+anbaphish@users.noreply.github.com>
2022-08-01 15:55:07 -0700

    Optimized further
    Changed timings to work

--- Linux.py
+++ Linux.py
@@ -1,15 +1,15 @@
 
 ##import keyboard
-
 ## used a duckyscript to python thing for the basic portions
-
-
-## [wastes 100% of cpu when doing if --> ]if keyboard.is_pressed('left alt'):
 import pyautogui
 import time
-pyautogui.hotkey("win","T")
+time.sleep(3)
+pyautogui.hotkey("winleft")
+time.sleep(0.4) #best timing so it doesn't miss from lag
+pyautogui.hotkey( "T")
+time.sleep(.01) #can be very fast since it's just opening
 pyautogui.hotkey("enter")
-time.sleep(0.1)
-pyautogui.typewrite("neofetch", interval=0.02)
+time.sleep(0.3) #longer for term to open
+pyautogui.typewrite("neofetch", interval=0.0)
 pyautogui.hotkey("enter")
 exit()
